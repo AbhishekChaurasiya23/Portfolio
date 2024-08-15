b7d291391bc209d5a3efd248f28b10ba95d414b1
AbhishekChaurasiya23 <chaurasiya2308@gmail.com>
2024-08-16 02:00:33 +0530

    PreLoader Done

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Cursor Animation
 var main = document.querySelector("#main");
 var cursor = document.querySelector("#cursor");
 
@@ -8,4 +9,41 @@ main.addEventListener("mousemove",function(val){
         duration : 1,
         ease : 'back.out'
     })
+});
+
+
+// Pre Loader 
+const texts = ["Video Editor", "Wanderlust", "Health Nut","Web Developer"];
+let index = 0;
+
+function changeText() {
+    const changingText = document.querySelector('#textscroll');
+    changingText.textContent = texts[index];
+    index = (index + 1) % texts.length;
+}
+
+setInterval(changeText,400);
+
+let tl = gsap.timeline()
+
+tl.to("#topHeading",{
+    height : 0,
+    duration : 4,
+    ease : Expo.easeInOut
+
 })
+.to("#green",{
+    height : "100%",
+    duration : 2,
+    ease : Expo.easeInOut
+
+})
+.to("#main-content",{
+    height : "100%",
+    duration : 2,
+    delay: -1.8,
+    ease : Expo.easeInOut
+
+})
+
+
